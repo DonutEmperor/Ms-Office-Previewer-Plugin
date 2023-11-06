9b977acb1ae3cf9019881dae96e3d1c33c2872b5
maxsonjoget <maxson.chen@joget.com>
2023-11-06 12:50:01 +0800

    Push maven project to remote repo

---
+++ src/main/java/org/marketplace/joget/ms/MsOfficePreviewer.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.marketplace.joget.ms;
+
+/**
+ *
+ * @author Maxson
+ */
+public class MsOfficePreviewer {
+    
+}
